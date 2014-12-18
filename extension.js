b3a2c8af883cbc075b345d52e4b4136bc7e5a029
Alex Van Camp <email@alexvan.camp>
2014-12-18 12:42:38 -0600

    remove redundant singleton code

--- extension.js
+++ extension.js
@@ -1,16 +1,8 @@
 'use strict';
 
-var nodecg = {};
-
-function Filter(extensionApi) {
-    if (Filter.prototype._singletonInstance) {
-        return Filter.prototype._singletonInstance;
-    }
-    Filter.prototype._singletonInstance = this;
-
-    nodecg = extensionApi;
+function Filter(nodecg) {
     this.wordfilter = require('./extension/wordfilter')(nodecg);
     this.emailfilter = require('./extension/emailfilter')(nodecg);
 }
 
-module.exports = function(extensionApi) { return new Filter(extensionApi) };
+module.exports = function(nodecg) { return new Filter(nodecg) };
