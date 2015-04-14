49bae95fcabcdaddb94361172f0b5f60f1732654
Alex Van Camp <email@alexvan.camp>
2015-04-14 11:19:46 -0500

    Use replicants, get ready for 0.6

--- extension/emailfilter.js
+++ extension/emailfilter.js
@@ -1,10 +1,8 @@
 'use strict';
 // Exports an emailfilter singleton
 
-var db = require('./db');
-var Q = require('q');
 var nodecg = {};
-var ID = '_emailblacklist';
+var emailBlacklist;
 
 function Emailfilter(extensionApi) {
     if (Emailfilter.prototype._singletonInstance) {
@@ -13,58 +11,14 @@ function Emailfilter(extensionApi) {
     Emailfilter.prototype._singletonInstance = this;
 
     nodecg = extensionApi;
-    var self = this;
-
-    this.init()
-        .then(function(blacklist) {
-            nodecg.declareSyncedVar({ variableName: 'emailBlacklist',
-                initialVal: blacklist.addresses,
-                setter: function(newVal) {
-                    self.write(newVal);
-                }
-            });
-        })
-        .fail(function(err) {
-            throw err;
-        });
+    emailBlacklist = nodecg.Replicant('emailBlacklist', { defaultValue: [] });
 }
 
-Emailfilter.prototype.init = function() {
-    var deferred = Q.defer();
-    db.findOne({ _id: ID }, function (err, doc) {
-        if (err) {
-            deferred.reject(new Error(err));
-        } else if (doc === null) {
-            // If the train isn't in the DB, make a new one with defaults
-            var defaultList = { _id: ID, addresses: [] };
-
-            db.insert(defaultList, function (err, newDoc) {
-                if (err) {
-                    deferred.reject(new Error(err));
-                } else {
-                    deferred.resolve(newDoc);
-                }
-            });
-        } else {
-            // Else, return the train already present in the DB
-            deferred.resolve(doc);
-        }
-    });
-    return deferred.promise;
-};
-
-Emailfilter.prototype.write = function(addresses) {
-    db.update({ _id: ID }, { addresses: addresses }, { upsert: true }, function (err) {
-        if (err) nodecg.log.error(err.stack);
-    });
-};
-
 Emailfilter.prototype.blacklisted = function (string) {
-    var blacklist = nodecg.variables.emailBlacklist;
+    var blacklist = emailBlacklist.value;
     if (!blacklist || !string) {
         return false;
     }
-
     return blacklist.indexOf(string.toLowerCase()) > -1;
 };
 
--- extension/wordfilter.js
+++ extension/wordfilter.js
@@ -1,10 +1,8 @@
 'use strict';
 // Exports an wordfilter singleton
 
-var db = require('./db');
-var Q = require('q');
 var nodecg = {};
-var ID = '_wordblacklist';
+var wordBlacklist;
 
 function Wordfilter(extensionApi) {
     if (Wordfilter.prototype._singletonInstance) {
@@ -13,65 +11,15 @@ function Wordfilter(extensionApi) {
     Wordfilter.prototype._singletonInstance = this;
 
     nodecg = extensionApi;
-    var self = this;
-
-    this.init()
-        .then(function(blacklist) {
-            nodecg.declareSyncedVar({ variableName: 'wordBlacklist',
-                initialVal: blacklist.words,
-                setter: function(newVal) {
-                    self.write(newVal);
-                }
-            });
-        })
-        .fail(function(err) {
-            throw err;
-        });
+    wordBlacklist = nodecg.Replicant('wordBlacklist', { defaultValue: [] });
 }
 
-Wordfilter.prototype.init = function() {
-    var deferred = Q.defer();
-    db.findOne({ _id: ID }, function (err, doc) {
-        if (err) {
-            deferred.reject(new Error(err));
-        } else if (doc === null) {
-            // If the train isn't in the DB, make a new one with defaults
-            var defaultList = { _id: ID, words: [] };
-
-            db.insert(defaultList, function (err, newDoc) {
-                if (err) {
-                    deferred.reject(new Error(err));
-                } else {
-                    deferred.resolve(newDoc);
-                }
-            });
-        } else {
-            // Else, return the train already present in the DB
-            deferred.resolve(doc);
-        }
-    });
-    return deferred.promise;
-};
-
-Wordfilter.prototype.write = function(words) {
-    db.update({ _id: ID }, { words: words }, { upsert: true }, function (err) {
-        if (err) nodecg.log.error(err.stack);
-    });
-};
-
 Wordfilter.prototype.blacklisted = function (string) {
-    var blacklist = nodecg.variables.wordBlacklist;
+    var blacklist = wordBlacklist.value;
     if (!blacklist || !string) {
         return false;
     }
-
-    for (var i = 0; i < blacklist.length; i++) {
-        if (string.toLowerCase().indexOf(blacklist[i]) >= 0) {
-            return true;
-        }
-    }
-
-    return false;
+    return blacklist.indexOf(string.toLowerCase()) > -1;
 };
 
 module.exports = function(extensionApi) { return new Wordfilter(extensionApi); };
