b2f25579b7cb571aed44e023b4017dd0588082ab
Alex Van Camp <email@alexvan.camp>
2015-05-24 16:38:57 -0500

    Make the wordfilter actually do something.

--- extension/emailfilter.js
+++ extension/emailfilter.js
@@ -1,25 +1,11 @@
 'use strict';
-// Exports an emailfilter singleton
 
-var nodecg = {};
-var emailBlacklist;
-
-function Emailfilter(extensionApi) {
-    if (Emailfilter.prototype._singletonInstance) {
-        return Emailfilter.prototype._singletonInstance;
-    }
-    Emailfilter.prototype._singletonInstance = this;
-
-    nodecg = extensionApi;
-    emailBlacklist = nodecg.Replicant('emailBlacklist', { defaultValue: [] });
-}
-
-Emailfilter.prototype.blacklisted = function (string) {
-    var blacklist = emailBlacklist.value;
-    if (!blacklist || !string) {
-        return false;
-    }
-    return blacklist.indexOf(string.toLowerCase()) > -1;
+module.exports = function(nodecg) {
+    var emailBlacklist = nodecg.Replicant('emailBlacklist', { defaultValue: [] });
+    return function (string) {
+        var blacklist = emailBlacklist.value;
+        if (!blacklist || !string) return false;
+        string = string.toLowerCase();
+        return blacklist.indexOf(string.toLowerCase()) > -1;
+    };
 };
-
-module.exports = function(extensionApi) { return new Emailfilter(extensionApi); };
--- extension/wordfilter.js
+++ extension/wordfilter.js
@@ -1,25 +1,17 @@
 'use strict';
-// Exports an wordfilter singleton
 
-var nodecg = {};
-var wordBlacklist;
-
-function Wordfilter(extensionApi) {
-    if (Wordfilter.prototype._singletonInstance) {
-        return Wordfilter.prototype._singletonInstance;
-    }
-    Wordfilter.prototype._singletonInstance = this;
-
-    nodecg = extensionApi;
-    wordBlacklist = nodecg.Replicant('wordBlacklist', { defaultValue: [] });
-}
-
-Wordfilter.prototype.blacklisted = function (string) {
-    var blacklist = wordBlacklist.value;
-    if (!blacklist || !string) {
-        return false;
-    }
-    return blacklist.indexOf(string.toLowerCase()) > -1;
+module.exports = function(nodecg) {
+    var wordBlacklist = nodecg.Replicant('wordBlacklist', { defaultValue: [] });
+    return function (string) {
+        var match = false;
+        var blacklist = wordBlacklist.value;
+        if (!blacklist || !string) return false;
+        string = string.toLowerCase();
+        blacklist.some(function(word) {
+            word = word.toLowerCase();
+            if (string.indexOf(word) >= 0) match = true;
+            return match;
+        });
+        return match;
+    };
 };
-
-module.exports = function(extensionApi) { return new Wordfilter(extensionApi); };
